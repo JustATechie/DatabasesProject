cfd49933d61d8da4019056a1d68bc4553185b9b7
JustATechie <47167717+JustATechie@users.noreply.github.com>
2022-04-14 20:09:04 -0400

    made location-small with only CA

--- sql/production/setup-small.sql
+++ sql/production/setup-small.sql
@@ -306,10 +306,10 @@ CREATE TABLE SchoolFoodPrograms(
 /* Load data into tables from files. */
 
 /* Location Data */
-LOAD DATA LOCAL INFILE './DatabasesProject/data/final-small/Location.txt' INTO TABLE Location FIELDS TERMINATED BY ',';
+LOAD DATA LOCAL INFILE './data/final-small/Location-small.csv' INTO TABLE Location FIELDS TERMINATED BY ',';
 
 /* Data Type */
-LOAD DATA LOCAL INFILE './DatabasesProject/data/final-small/DataType.txt' INTO TABLE DataType FIELDS TERMINATED BY ',';
+LOAD DATA LOCAL INFILE './data/final-small/DataType.csv' INTO TABLE DataType FIELDS TERMINATED BY ',';
 
 /* Food Legislation Data */
 #LOAD DATA LOCAL INFILE './DatabasesProject/data/final/FoodLegislation.csv' INTO TABLE FoodLegislation FIELDS TERMINATED BY ',';
@@ -318,7 +318,7 @@ LOAD DATA LOCAL INFILE './DatabasesProject/data/final-small/DataType.txt' INTO T
 #LOAD DATA LOCAL INFILE './DatabasesProject/data/final/PopulationStats.csv' INTO TABLE PopulationStats FIELDS TERMINATED BY ',';
 
 /* Consumption Stats Data */
-LOAD DATA LOCAL INFILE './DatabasesProject/data/final/ConsumptionStats.csv' INTO TABLE ConsumptionStats FIELDS TERMINATED BY ',';
+LOAD DATA LOCAL INFILE './data/final-small/ConsumptionStats.csv' INTO TABLE ConsumptionStats FIELDS TERMINATED BY ',';
 
 /* Metabolic Disease Data */
 #LOAD DATA LOCAL INFILE './DatabasesProject/data/final/MetabolicDisease.csv' INTO TABLE MetabolicDisease FIELDS TERMINATED BY ',';
