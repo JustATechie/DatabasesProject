0aac771f6e38373cfababcf1759af95f73ac93b1
JustATechie <47167717+JustATechie@users.noreply.github.com>
2022-04-26 18:08:59 -0400

    completed query #4 (and 5 but they are similar so counting as one)

--- sql/production/queries.sql
+++ sql/production/queries.sql
@@ -33,8 +33,7 @@ Order by Year;
 
 /**
   * NEW QUESTION:
-  * The state with the highest sugar intake, how many residents are enrolled in food assistance programs? (Specifically in CA)
-  *
+  * In the year with the highest sugar intake, how many people were enrolled in FoodAssistance programs? (Specifically in CA)
   *
   */
 
@@ -42,16 +41,38 @@ select ConsumptionStats.Year as 'Year with Maximum Sugar Intake', MAX(SugarIntak
        (select SUM(numEnrolled) as 'EnrollmentNumbers' from FoodAssistance left join Location on FoodAssistance.LocationID = Location.LocationID and Location.State='California' where Year=ConsumptionStats.Year) as numEnrolled
 from ConsumptionStats left join Location on ConsumptionStats.LocationID = Location.LocationID and Location.State='California';
 
-
+# we could try to modify the following query to select the state with the highest sugar intake.
+# select Location.State as 'State', ConsumptionStats.Year as 'Year with Maximum Sugar Intake', MAX(SugarIntake) as 'Maximum Sugar Intake',
+#        (select SUM(numEnrolled) as 'EnrollmentNumbers' from FoodAssistance left join Location on FoodAssistance.LocationID = Location.LocationID where Location.State=Location.State and Year=ConsumptionStats.Year) as numEnrolled
+# from ConsumptionStats left join Location on ConsumptionStats.LocationID = Location.LocationID;
 
 
 
 /**
   * NEW QUESTION:
   * Does the state with the highest number of people enrolled in food assistance programs also have the highest number of students
-  * enrolled in school programs? (Specifically in CA)
+  * enrolled in school programs in the same year? (Specifically in CA)
+  *
+  * Currently, designed to be displayed in two different tables.
+  */
+
+select State, Year, MAX(sumEnrolled)
+from (select State, Year, SUM(FoodAssistance.numEnrolled) as 'sumEnrolled' from FoodAssistance left join Location on FoodAssistance.locationID=Location.locationID where State='California' group by year order by sumEnrolled DESC) as enrollmentInfo;
+
+select State, Year, MAX(sumEnrolled)
+from (select State, Year, SUM(SchoolFoodPrograms.numStudents) as 'sumEnrolled' from SchoolFoodPrograms left join Location on SchoolFoodPrograms.locationID=Location.locationID where State='California' group by year order by sumEnrolled DESC) as enrollmentInfo;
+
+
+/** SIMILAR QUESTION TO LAST BUT CHANGE A LITTLE TO MAKE IT ONE QUERY.
+  * In the year with the highest number of people enrolled in Food assistance programs, how many students were enrolled in the
+  * school food programs in that same year?
   *
   */
+
+select FoodAssistanceSummed.State, FoodAssistanceSummed.Year, MAX(FoodAssistanceSummed.sumEnrolled) as 'Number of people enrolled in Food Assistance programs',
+       (select SUM(numStudents) as 'sumStudents' from SchoolFoodPrograms left join Location on SchoolFoodPrograms.locationID=Location.locationID where Year=FoodAssistanceSummed.Year and State=FoodAssistanceSummed.State Group by year order by sumStudents DESC)
+                                                                                                    as 'Number of students enrolled in SchoolFoodPrograms'
+from (select State, Year, SUM(numEnrolled) as 'sumEnrolled' from FoodAssistance left join Location on FoodAssistance.locationID=Location.locationID Group by year Order By sumEnrolled DESC) as FoodAssistanceSummed;
 
 
 
