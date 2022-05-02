24021f731d48dc413240294aca2b3fb275a01594
JustATechie <47167717+JustATechie@users.noreply.github.com>
2022-05-02 13:31:38 -0400

    💡 added needed php files.

---
+++ php/conf.php
@@ -0,0 +1,8 @@
+<?php
+//these are credentials which will be used by open.php to create connection to dbase
+//make updates here as noted below, and save this file as conf.php
+$dbhost = ""; // database host
+$dbuser = "";    // username on db host: replace 22sp_jhopkin1 with yours
+$dbpass = "";            // password on db host: replace ***** with yours
+$dbname = ""; // the db's name: replace 22sp_jhopkin1_db with yours
+?>
---
+++ php/test.php
@@ -0,0 +1,10 @@
+<head><title>PHP PreparedStatement example</title></head>
+<body>
+
+<?php
+include 'open.php';
+
+//Override the PHP configuration file to display all errors
+//This is useful during development but generally disabled before release
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', true);
