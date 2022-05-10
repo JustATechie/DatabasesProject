118590b7437b8d0810e36b9b468ba72e84e455db
JustATechie <47167717+JustATechie@users.noreply.github.com>
2022-05-10 03:21:26 -0400

    Q3 and 4 complete.

---
+++ public_html/php/questions/Q3/Q3.php
@@ -0,0 +1,47 @@
+<!--Q3: In the year with the highest sugar intake, how many people were enrolled in FoodAssistance programs?-->
+<?php include "../../templates/questions/question.php" ?>
+<!-------------------------- ONLY MAKE CHANGES BELOW THIS LINE -------------------------->
+<head>
+    <!-- Tab Title -->
+    <title>Sugar Rates and Food Assistance Programs</title>
+</head>
+<body>
+<br>
+<!-- Global variables -->
+<?php
+GLOBAL $Q3State;
+GLOBAL $Q3Year;
+GLOBAL $Q3Sugar;
+GLOBAL $Q3Enroll;
+?>
+
+<h4> In the year with the highest sugar intake, how many people were enrolled in Food Assistance programs? </h4>
+
+<form method="POST" action="">
+    <div class="form-inline">
+        <button class="btn btn-primary" name="getInfo">Submit</button>
+    </div>
+</form>
+
+<br>
+<br>
+
+<!-- Get general info! -->
+<?php
+
+$Object = include 'getData.php';
+
+if($Object->num_rows > 0){
+    foreach($Object as $row){
+        $Q3State=$row['State'];
+        $Q3Year=$row['Year'];
+        $Q3Sugar=$row['SugarIntake'];
+        $Q3Enroll=$row['EnrollNum'];
+
+    }
+    echo "<h5>" . $Q3State . " had the highest sugar intake across all genders and ages in " . $Q3Year . ". In this year, the average person consumed " . $Q3Sugar . " sugary drinks per day. In this same year, " . $Q3Enroll . " people were enrolled in Food Assistance programs." . "</h5>";
+
+}
+?>
+
+
---
+++ public_html/php/questions/example/example.php
@@ -0,0 +1,24 @@
+<!--Q#:-->
+<?php include "../../templates/questions/question.php" ?>
+<!-------------------------- ONLY MAKE CHANGES BELOW THIS LINE -------------------------->
+<head>
+    <!-- Tab Title -->
+    <title></title>
+</head>
+<body>
+<br>
+<!-- Global variables -->
+<?php
+
+?>
+
+<h4></h4>
+
+<form method="POST" action="">
+    <div class="form-inline">
+        <button class="btn btn-primary" name="getInfo">Submit</button>
+    </div>
+</form>
+
+<br>
+<br>
