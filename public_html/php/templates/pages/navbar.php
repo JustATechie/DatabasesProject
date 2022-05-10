250967dd25b8ee015c13c704ef7a656589a8ff46
JustATechie <47167717+JustATechie@users.noreply.github.com>
2022-05-10 14:44:49 -0400

    fixed issue with navbar in pages

--- public_html/php/templates/pages/navbar.php
+++ public_html/php/templates/pages/navbar.php
@@ -1,14 +1,14 @@
 <!-------------------- NavBar -------------------->
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-        <a class="navbar-brand" href="../../../index.html">
+        <a class="navbar-brand" href="../../index.html">
             <img src="/public_html/assets/seal.png" alt="" width="70" height="50" class="d-inline-block align-text-center">
             Databases Project
         </a>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="../../../index.html">Home</a>
+                    <a class="nav-link active" aria-current="page" href="../../index.html">Home</a>
                 </li>
             </ul>
         </div>
