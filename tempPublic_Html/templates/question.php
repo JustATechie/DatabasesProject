67fef75e22c3ea6a0cc7c78b1b56f5a122bd6bd1
JustATechie <47167717+JustATechie@users.noreply.github.com>
2022-05-10 09:54:00 -0400

    git push new temppublichtml for debugging

---
+++ tempPublic_Html/templates/question.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Include head information from template file -->
+    <?php include('head.php'); ?>
+
+</head>
+
+<body>
+<!-------------------- NavBar -------------------->
+<?php include('navbar.php'); ?>
+<!-------------------- End NavBar -------------------->
+
+<?php
+require '../open.php';
+//Override the PHP configuration file to display all errors
+//This is useful during development but generally disabled before release
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', true);
+?>
