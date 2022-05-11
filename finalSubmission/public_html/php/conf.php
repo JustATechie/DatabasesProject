fc8479811a7a5e8bdbb0a6eea667feea2691649f
JustATechie <47167717+JustATechie@users.noreply.github.com>
2022-05-10 22:46:12 -0400

    final updates!

--- finalSubmission/public_html/php/conf.php
+++ finalSubmission/public_html/php/conf.php
@@ -1,8 +1,8 @@
 <?php
 //these are credentials which will be used by open.php to create connection to dbase
 //make updates here as noted below, and save this file as conf.php
-$dbhost = "localhost"; // database host
-$dbuser = "root";    // username on db host: replace 22sp_jhopkin1 with yours
-$dbpass = "StocksandTechie$7845";            // password on db host: replace ***** with yours
-$dbname = "Project"; // the db's name: replace 22sp_jhopkin1_db with yours
+$dbhost = ""; // database host
+$dbuser = "";    // username on db host: replace 22sp_jhopkin1 with yours
+$dbpass = "";            // password on db host: replace ***** with yours
+$dbname = ""; // the db's name: replace 22sp_jhopkin1_db with yours
 ?>
